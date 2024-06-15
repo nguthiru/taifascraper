067dd0f7a7c7fed6ba99398545ee4507839b5640
nguthiru <eddiendiritu@gmail.com>
2024-06-15 18:06:46 +0300

    initial commit and dependencies setup

---
+++ lib/taifascraper.ex
@@ -0,0 +1,18 @@
+defmodule Taifascraper do
+  @moduledoc """
+  Documentation for `Taifascraper`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Taifascraper.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/taifascraper_test.exs
@@ -0,0 +1,8 @@
+defmodule TaifascraperTest do
+  use ExUnit.Case
+  doctest Taifascraper
+
+  test "greets the world" do
+    assert Taifascraper.hello() == :world
+  end
+end
